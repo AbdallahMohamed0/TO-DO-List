027d6a615ae1e7c39622617ba80b9b778337e19c
Abdallah Mohamed <113191982+AbdallahMohamed0@users.noreply.github.com>
2023-03-15 18:18:38 +0200

    Done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const boards = document.getElementsByClassName("board-box");
-const boardsArr = [...boards];
+const boardsArr = [...document.getElementsByClassName("board-box")];
 const boardsShort = ["not-started", "in-progress", "completed"];
+const lists = [...document.getElementsByTagName("ul")];
 
 // Save to Local Storage
 const nsBoard = JSON.parse(localStorage.getItem("not-started")) || [];
@@ -10,6 +10,9 @@ const boardsStorage = [nsBoard, ipBoard, compBoard];
 
 let startBoardIdx;
 let draggedItemIdx;
+let draggedItem;
+let droppedItemIdx;
+let endBoardIdx;
 
 /********************************/
 /* Create a task LI element */
@@ -64,7 +67,7 @@ function disabledOnBlur(input, listNum, liNum) {
   input.disabled = true;
   const boardStore = boardsStorage[listNum];
   boardStore[liNum] = input.value;
-  console.log(liNum, boardStore);
+
   localStorage.setItem(boardsShort[listNum], JSON.stringify(boardStore));
 }
 
@@ -91,18 +94,102 @@ function editTaskInput(li) {
 /******************/
 /* DRAG & DROP */
 /******************/
+
+function dragStart(e) {
+  let targ;
+  if (e.target.tagName === "LI") {
+    targ = e.target;
+  } else if (e.target.tagName === "INPUT") {
+    targ = e.target.closest("li");
+  }
+
+  if (targ) {
+    startBoardIdx = lists.findIndex((list) => list === targ.closest("ul"));
+
+    draggedItemIdx = [...e.currentTarget.getElementsByTagName("li")].findIndex(
+      (el) => el === targ
+    );
+
+    draggedItem =
+      lists[startBoardIdx].getElementsByTagName("li")[draggedItemIdx];
+
+    draggedItem.classList.add("dragged");
+  }
+}
+
+function touchDrop(target) {
+  lists.forEach((list) =>
+    [...list.getElementsByTagName("li")].forEach((li) =>
+      li.classList.remove("target")
+    )
+  );
+  draggedItem = lists[startBoardIdx].getElementsByTagName("li")[draggedItemIdx];
+
+  draggedItem.classList.remove("dragged");
+  let droppedItem = draggedItem.cloneNode(true);
+
+  boardsStorage[startBoardIdx].splice(draggedItemIdx, 1);
+
+  localStorage.setItem(
+    boardsShort[startBoardIdx],
+    JSON.stringify(boardsStorage[startBoardIdx])
+  );
+
+  if (target === "INPUT") {
+    let liEl = lists[endBoardIdx].getElementsByTagName("li")[droppedItemIdx];
+    boardsStorage[endBoardIdx].splice(
+      droppedItemIdx + 1,
+      0,
+      droppedItem.getElementsByClassName("input")[0].value
+    );
+
+    // Local Storage
+    localStorage.setItem(
+      boardsShort[endBoardIdx],
+      JSON.stringify(boardsStorage[endBoardIdx])
+    );
+
+    // Inserting element
+    liEl.insertAdjacentElement("afterend", droppedItem);
+  } else if (target === "UL") {
+    lists[endBoardIdx].append(droppedItem);
+
+    // Local Storage
+    boardsStorage[endBoardIdx].push(
+      droppedItem.getElementsByClassName("input")[0].value
+    );
+
+    localStorage.setItem(
+      boardsShort[endBoardIdx],
+      JSON.stringify(boardsStorage[endBoardIdx])
+    );
+  }
+
+  droppedItem
+    .getElementsByClassName("btn remove")[0]
+    .addEventListener(
+      "click",
+      removeTaskElement.bind(null, endBoardIdx, droppedItemIdx + 1, droppedItem)
+    );
+
+  droppedItem
+    .getElementsByClassName("btn edit")[0]
+    .addEventListener("click", editTaskInput.bind(null, droppedItem));
+
+  const input = droppedItem.getElementsByClassName("input")[0];
+
+  input.addEventListener(
+    "blur",
+    disabledOnBlur.bind(null, input, endBoardIdx, droppedItemIdx + 1 || 0)
+  );
+
+  draggedItem.remove();
+}
+
 function dragDropEvents(board) {
   const boardList = board.getElementsByClassName("list")[0];
 
-  boardList.addEventListener("dragstart", (e) => {
-    startBoardIdx = boardsArr.findIndex(
-      (board) => board === e.currentTarget.closest("div")
-    );
-
-    draggedItemIdx = [...e.currentTarget.getElementsByTagName("li")].findIndex(
-      (el) => el === e.target
-    );
-  });
+  boardList.addEventListener("dragstart", dragStart);
 
   boardList.addEventListener("dragover", (e) => {
     e.preventDefault();
@@ -112,6 +199,11 @@ function dragDropEvents(board) {
     const target = e.target;
     if (e.target.tagName === "INPUT") {
       target.closest("li").classList.add("target");
+    } else if (
+      e.target.tagName === "UL" &&
+      !e.target.getElementsByTagName("li").length
+    ) {
+      target.classList.add("target");
     }
   });
 
@@ -119,16 +211,20 @@ function dragDropEvents(board) {
     const target = e.target;
     if (target.tagName === "INPUT") {
       target.closest("li").classList.remove("target");
+    } else if (e.target.tagName === "UL") {
+      target.classList.remove("target");
     }
   });
 
+  // Drag end
+  boardList.addEventListener("dragend", () => {
+    draggedItem.classList.remove("dragged");
+  });
+
   boardList.addEventListener("drop", (e) => {
-    let endBoardIdx;
-    let droppedItemIdx;
     let droppedItem;
-    let draggedItem = boardsArr[startBoardIdx]
-      .getElementsByTagName("ul")[0]
-      .getElementsByTagName("li")[draggedItemIdx];
+
+    draggedItem.classList.remove("dragged");
 
     let liEl;
 
@@ -176,8 +272,8 @@ function dragDropEvents(board) {
 
         liEl.insertAdjacentElement("afterend", droppedItem);
       } else if (e.target.tagName === "UL") {
-        console.log("UL");
         e.target.append(droppedItem);
+        e.target.classList.remove("target");
 
         boardsStorage[endBoardIdx].push(
           droppedItem.getElementsByClassName("input")[0].value
@@ -207,14 +303,103 @@ function dragDropEvents(board) {
         .addEventListener("click", editTaskInput.bind(null, droppedItem));
 
       const input = droppedItem.getElementsByClassName("input")[0];
-      console.log(droppedItemIdx);
 
       input.addEventListener(
         "blur",
         disabledOnBlur.bind(null, input, endBoardIdx, droppedItemIdx + 1 || 0)
       );
+    }
+  });
+}
 
-      console.log(boardsStorage[endBoardIdx]);
+function dragDropTouchEvents(board) {
+  const boardList = board.getElementsByClassName("list")[0];
+
+  boardList.addEventListener("touchstart", dragStart);
+
+  board.addEventListener("touchend", (e) => {
+    if (e.target.tagName === "INPUT") {
+      draggedItem.classList.remove("dragged");
+
+      if (
+        endBoardIdx >= 0 &&
+        (endBoardIdx !== startBoardIdx || droppedItemIdx !== draggedItemIdx)
+      ) {
+        lists[endBoardIdx].classList.remove("target");
+        if (droppedItemIdx >= 0) {
+          touchDrop("INPUT");
+        } else {
+          touchDrop("UL");
+        }
+      }
+    }
+  });
+
+  board.addEventListener("touchmove", (e) => {
+    if (e.changedTouches[0].target.tagName === "INPUT") {
+      e.preventDefault();
+      const currY = e.changedTouches[0].clientY;
+      const currX = e.changedTouches[0].clientX;
+
+      const { clientHeight } = document.documentElement;
+      if (currY >= clientHeight * 0.75) {
+        window.scrollBy({
+          top: clientHeight * 0.05,
+          behavior: "smooth",
+        });
+      } else if (currY <= clientHeight * 0.25) {
+        window.scrollBy({
+          top: -clientHeight * 0.05,
+          behavior: "smooth",
+        });
+      }
+
+      // Calculting lists dimensions
+      const rects = [];
+      const lists = [...document.getElementsByTagName("ul")];
+      lists.forEach((list) => rects.push(list.getBoundingClientRect()));
+
+      const idx = rects.findIndex((rect) => {
+        return (
+          currY >= rect.top &&
+          currY <= rect.bottom &&
+          currX >= rect.left &&
+          currX <= rect.right
+        );
+      });
+
+      let liEls;
+      let liIdx;
+      if (idx >= 0) {
+        if (endBoardIdx && endBoardIdx !== idx) {
+          lists[endBoardIdx].classList.remove("target");
+        }
+        endBoardIdx = idx;
+        liEls = [...lists[idx].getElementsByTagName("li")];
+
+        liIdx = liEls.findIndex((el) => {
+          const { top, bottom } = el.getBoundingClientRect();
+          return currY >= top && currY <= bottom + 10;
+        });
+      }
+
+      const allLis = [...document.getElementsByTagName("li")];
+
+      if (e.target.tagName === "INPUT") {
+        if (liIdx !== droppedItemIdx) {
+          allLis.forEach((li) => li.classList.remove("target"));
+          droppedItemIdx = liIdx;
+          if (startBoardIdx !== endBoardIdx || liIdx !== draggedItemIdx) {
+            if (liIdx >= 0) {
+              lists[endBoardIdx].classList.remove("target");
+              liEls[liIdx].classList.add("target");
+            } else {
+              !lists[endBoardIdx].getElementsByTagName("li").length &&
+                lists[endBoardIdx].classList.add("target");
+            }
+          }
+        }
+      }
     }
   });
 }
@@ -227,13 +412,12 @@ function app() {
     const boardShort = boardsShort[i];
     const oldBoard = boardsStorage[i];
 
-    const boardList = board.getElementsByClassName("list")[0];
     const addBtn = board.getElementsByClassName("btn btn--add")[0];
 
     if (oldBoard.length > 0) {
       for (let j = 0; j < oldBoard.length; j++) {
         const text = oldBoard[j];
-        const newTask = createTaskEl(i, j, text);
+        createTaskEl(i, j, text);
       }
     }
 
@@ -251,6 +435,7 @@ function app() {
 
     // Enable drag-and-drop functionality
     dragDropEvents(board);
+    dragDropTouchEvents(board);
   }
 }
 
